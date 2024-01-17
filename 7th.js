1b75ed241b58c5d96c4c8d25863c6e05214dcfc4
foxyasha <zxcfoxie13@mail.ru>
2024-01-17 21:23:27 +0700

    update

--- 7th.js
+++ 7th.js
@@ -1,6 +1,6 @@
 let month = Math.floor(Math.random() * 12) + 1
 let year = 2024
-const season = () =>{
+const season = (month) =>{
     if(month == 12 || month < 3){
         return 'Зима'
     }
@@ -12,7 +12,7 @@ const season = () =>{
     }
     else return 'Осень'
 }
-const leapYear = () =>{
+const leapYear = (year) =>{
     if (year % 4 === 0){
         return 'Високосный год'
     }
